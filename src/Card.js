20cc6f2422811612153bc2f2437f932086896082
FredThx <fredthx@gmail.com>
2018-03-18 18:51:06 +0100

    Init

---
+++ src/Card.js
@@ -0,0 +1,29 @@
+﻿import React from 'react'
+import PropTypes from 'prop-types'
+
+import './Card.css'
+
+const HIDDEN_SYMBOL = '?'
+
+const Card = ({card, feedback, onClick}) => (
+	//{/* On ne peut pas ecrire 'card + feedback' car  il n'y a pas d'interprétation pour les nom de  props. On utilise alors les template*/}
+	<div className={'card &{feedback]'} onClick = {() => onClick(card)}>
+		<span className="symbol">
+			{feedback === 'hidden' ? HIDDEN_SYMBOL : card}
+		</span>
+	</div>
+)
+
+Card.propType = {
+	card:PropTypes.string.isRequired,
+	feedback : PropTypes.oneOf([
+		'visible',
+		'hidden',
+		'justMatched',
+		'justMismatched'
+	]).isRequired,
+	onclick:PropTypes.func.isRequired
+}
+
+
+export default Card
---
+++ src/MonButton.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const MonButton = ()=> (
+	<button onClick={()=>console.log('Salut')}>
+		Clique ici !
+	</button>
+)
+
+
+export default MonButton
